d8a53caead3202fe55d91dddbdf3f1901c7f5221
Vinitziin <vinicius.tiago.65154@a.fecaf.com.br>
2023-11-07 16:39:06 -0300

    Formulário Flask

--- git.py
+++ git.py
@@ -1,11 +1,22 @@
 # app.py
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template, request
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
 
-@app.route('/')
-def home():
-    return "Olá, Mundo!"
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your-secret-key'
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Nome de Usuário')
+    submit = SubmitField('Cadastrar')
+
+@app.route('/', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        return 'Usuário {} cadastrado com sucesso!'.format(username)
+    return render_template('register.html', form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
-False
